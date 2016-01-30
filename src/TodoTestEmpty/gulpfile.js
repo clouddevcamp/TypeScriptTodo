c306cc465854b3ec8415d28043ab67c03806e607
tahaipek <ti.tahaipek@gmail.com>
2016-01-30 03:26:03 +0200

    Update

--- src/TodoTestEmpty/gulpfile.js
+++ src/TodoTestEmpty/gulpfile.js
@@ -5,7 +5,11 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp'), //npm install gulp --save-dev
     less = require("gulp-less"), //npm install gulp-less --save-dev;
-    ts = require("gulp-typescript"); //npm install gulp-typescript --save-dev;
+    ts = require("gulp-typescript"), //npm install gulp-typescript --save-dev;
+    minifycss = require("gulp-minify-css"), //npm install gulp-minify-css --save-dev
+    rename = require("gulp-rename"), //npm install gulp-rename --save-dev
+    uglify = require("gulp-uglify"); //npm install gulp-uglify --save-dev
+
 
 
 var paths = {
@@ -16,10 +20,15 @@ var paths = {
     destCss: "wwwroot/css/"
 };
 
+
+
 gulp.task("compile:less", function () {
     console.log("less compile başladı.");
     return gulp.src(paths.srcLess + "main.less")
                .pipe(less())
+               .pipe(gulp.dest(paths.destCss))
+               .pipe(rename({ suffix: '.min' }))
+               .pipe(minifycss({ keepBreaks: true }))
                .pipe(gulp.dest(paths.destCss));
 
 });
@@ -31,6 +40,9 @@ gulp.task("compile:ts", function() {
     console.log("ts compile başladı.");
     return gulp.src(paths.srcTs + "main.ts")
                .pipe(ts({ out: "main.js" }))
+               .pipe(gulp.dest(paths.destTs))
+               .pipe(rename({ suffix: ".min" }))
+               .pipe(uglify())
                .pipe(gulp.dest(paths.destTs));
 
 });
